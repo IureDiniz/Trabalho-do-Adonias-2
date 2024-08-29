96838662ae03a7df56bb30e6698652fd2f2444ea
IureDiniz <139821101+IureDiniz@users.noreply.github.com>
2024-08-29 09:52:16 -0300

    Create questão2.c

---
+++ questão2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+    long int N[10], total = 0, pares = 0, impares = 0, maxRel = 0;
+    
+    //Recebe todos os inteiros no vetor N
+    int i;
+    for(i = 0; i < 10; i++){
+        scanf("%ld", &N[i]);
+        total += N[i];
+        
+        //Analisa e incrementa a quantidade de numeros pares ou impares
+        if(N[i]%2 == 0){
+            pares++;
+        }else{
+            impares++;
+        }
+        
+    }
+    
+    //Avalia se os inteiros nas extremidades sao maximos relativos
+    maxRel = (N[0] > N[1]) ? maxRel + 1 : maxRel;
+    
+    maxRel = (N[9] > N[8]) ? maxRel + 1 : maxRel;
+    
+    //Avalia se os outros inteiros sao maximos relativos
+    for(i = 1; i < 9; i++){
+        if(N[i] > N[i-1] && N[i] > N[i+1])
+            maxRel++;
+    }
+    
+    //Imprime a media dos inteiros, a quantidade de valores impares e pares e quantos numeros sao maximos relativo
+    printf("A media dos valores eh: %.2lf\n", total/10.0);
+    printf("A quantidade de valores impares eh: %ld\n", impares);
+    printf("A quantidade de valores pares eh: %ld\n", pares);
+    printf("A quantidade de maximos relativos eh: %ld\n", maxRel);
+    return 0;
+}
